d0c3879bed1fe8db72d9da81b1b70cf4e3fd8d1d
Ilma Arifiany <ilma@fabelio.com>
2020-09-16 00:08:03 +0700

    update pwa workbox

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-var cacheName = 'bbcode-editor';
-var filesToCache = [
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+
+const v = 'v2'
+const cached = {
+  prefetch: 'bbcode-' + v
+}
+const filesToCache = [
   '/',
   '/index.html',
   '/css/fa.css',
@@ -13,53 +18,93 @@ var filesToCache = [
   '/icons/icon-128x128.png',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
-  '/icons/icon-512x512.png',
   '/webfonts/fa-solid-900.eot',
   '/webfonts/fa-solid-900.svg',
   '/webfonts/fa-solid-900.ttf',
   '/webfonts/fa-solid-900.woff',
-  '/webfonts/fa-solid-900.woff2'
-];
+  '/webfonts/fa-solid-900.woff2',
+  'https://cdn.jsdelivr.net/g/filesaver.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/mammoth/1.4.9/mammoth.browser.min.js'
+]
 
-self.addEventListener('install', async event => {
-  const cache = await caches.open(cacheName + '-static-cache');
-  cache.addAll(filesToCache);
-});
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'js-cache',
+  })
+)
 
-self.addEventListener('message', event => {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting()
-  }
+workbox.routing.registerRoute(
+  /\.css$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'css-cache',
+  })
+)
+
+workbox.routing.registerRoute(
+  /\.(?:png|woff2|svg|eot|ttf|woff)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'image-font-cache',
+  })
+)
+
+workbox.precaching.precacheAndRoute(filesToCache)
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cached.prefetch).then((cache) => {
+      filesToCache.map((urlToPrefetch) => {
+        let url = new URL(urlToPrefetch, location.href)
+
+        url.search += (url.search ? '&' : '?') + 'cache-bust=' + Date.now()
+
+        const request = new Request(url, {
+          mode: 'no-cors'
+        })
+
+        return fetch(request).then((response) => {
+          if (response.status >= 400) {
+            throw new Error(urlToPrefetch + ': ' + response.statusText)
+          }
+
+          return cache.put(urlToPrefetch, response)
+        }).catch((error) => {
+          console.error(error)
+        })
+      })
+    }).catch((error) => {
+      console.error(error)
+    })
+  )
 })
 
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  const url = new URL(req.url);
+self.addEventListener('activate', (event) => {
+  const expectedCacheNames = Object.keys(cached).map((key) => cached[key])
 
-  if (url.origin === location.url) {
-    event.respondWith(cacheFirst(req));
-  } else {
-    event.respondWith(networkFirst(req));
-  }
-});
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (expectedCacheNames.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    )
+  )
+})
 
-async function cacheFirst(req) {
-  const cachedResponse = caches.match(req);
-  return cachedResponse || fetch(req);
-}
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response
+      }
 
-async function networkFirst(req) {
-  const cache = await caches.open(cacheName + '-dynamic-cache');
-
-  try {
-    const res = await fetch(req);
-    if (req.url.indexOf('http') !== 0) cache.put(req, res.clone());
-    return res;
-  } catch (error) {
-    return await cache.match(req);
-  }
-}
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
+      return fetch(event.request).then((response) => response).catch((error) => {
+        console.error('Fail:', error)
+        throw error
+      })
+    })
+  )
+})
